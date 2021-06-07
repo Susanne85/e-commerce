18ae94da71ea401bdf39e09f14549ac34019cf6f
Susanne Bilney <xkqk2010@gmail.com>
2021-06-07 16:48:33 +0800

    Ensured that products and categories cascade when deleted

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -7,10 +7,14 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
+  onDelete: 'CASCADE',  
 });
 
 // Categories have many Products
-Category.hasMany(Product);
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',  
+});
 
 Product.belongsToMany(Tag, {
   foreignKey: 'product_id',
